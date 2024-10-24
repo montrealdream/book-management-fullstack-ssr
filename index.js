9391a2537d0a5e4467c08269481a2c42d5391fac
coder-dom <giangtruong100702@gmail.com>
2024-10-25 00:03:26 +0700

    cài đặt file .env

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
+require('dotenv').config();
 const routerClient = require('./routes/admin/index.route');
 
 // configuration default
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 // router
 routerClient(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Dự án Quản Lý Sách`);
 })

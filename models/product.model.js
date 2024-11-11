ade57d0683eaceb496e9241e6557ac8e86205bb1
coder-dom <giangtruong100702@gmail.com>
2024-11-12 01:46:45 +0700

    cấu trúc lại model Product

--- models/product.model.js
+++ models/product.model.js
@@ -14,63 +14,20 @@ const COLLECTION_NAME = 'products';
 const productSchema = new mongoose.Schema(
     {
         title: { type: String, required: true },
-            
-        slug: {
-            type: String,
-            slug: "title", // tạo slug dựa vào title
-            unique: true   // tạo slug riêng biệt, tên giống nhau thì gắn thêm token ngẫu nhiên
-        },
-        
-        author: {
-            // tác giả của sách
-            type: String,
-            default:""
-        },     
-
-        category_id: { 
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'products-category'
-        },
-
-        thumbnail: {
-            type: Array,
-            default: [], // upload nhiều ảnh nên cần dùng mảng 
-            required: true
-        },
-
+        slug: { type: String,  slug: "title", unique: true },
+        author: {  type: String, default:"" },  // tác giả của sách
+        category_id: { type: mongoose.Schema.Types.ObjectId, ref: 'products-category'},
+        thumbnail: { type: Array,  default: [], required: true },
         description: String,
-
         price: { type: Number, required: true },
-        
         discountPercentage: { type: Number, default: 0 },
-        
         stock: {type: Number, required: true},
-        
-        position: {
-            type: Number,
-            min: 0,
-            required: true
-        },
-        
+        position: { type: Number, min: 0, required: true },
         sold: { type: Number, default: 0 }, // số lượng sản phẩm đã bán
-        
-        status: {
-            type: String,
-            enum: ['active', 'inactive'],
-            default: 'active'
-        },
-        
-        deleted: {
-            type:Boolean,
-            default: false // mặc định chưa xóa sản phẩm
-        },
-        
-        featured: {
-            type: String, 
-            enum: ['active', 'inactive'],
-            default: 'inactive'
-        }, // sản phẩm nổi bật
-
+        status: { type: String, enum: ['active', 'inactive'], default: 'active' },        
+        deleted: { type: Boolean, default: false}, // mặc định chưa xóa sản phẩm
+        featured: { type: String, enum: ['active', 'inactive'], default: 'inactive' }, // nổi bật
+            
         // Lưu giữ user
         createdBy : {
             userId: String,
